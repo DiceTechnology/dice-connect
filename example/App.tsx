b2bf0f20d45ccc1fa2b05a58e3bb39167a5cfb8e
Frederic Pinon <frederic.pinon@gmail.com>
2018-09-14 10:22:27 +0100

    chore: removing flowconfig

--- example/App.tsx
+++ example/App.tsx
@@ -1,33 +1,10 @@
 import * as React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { View } from 'react-native';
 
-type Props = {};
-export default class App extends React.Component<Props> {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-      </View>
+      <View/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
